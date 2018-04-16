6cd453a1855e978b99e440589a5810e30cfd9496
PBorasiya <parixit.borasia@gmail.com>
2018-04-16 00:10:30 -0700

    Read/write to file
    Student.txt file which has manual entries of student name saved so when we restart the program the data is still there.

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student["name"].title()
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 
@@ -18,11 +18,32 @@ def add_student(name,student_id = 32):
     students.append(student)
 
 
-student_list = get_students_titlecase()
+def save_file(student):
+    try:
+        f = open("students.txt","a")
+        f.write(student + "\n")
+        f.close()
+    except Exception:
+        print("Could not save file")
+
+
+def read_file():
+    try:
+        f = open("students.txt","r")
+        for student in f.readlines():
+            add_student(student)
+        f.close()
+    except Exception:
+        print("Could not read file")
+
+
+read_file()
+print_student_titlecase()
+
 
 student_name = input("Enter student name: ")
 student_id = input("Enter student ID: ")
 
 
 add_student(student_name,student_id)
-print_student_titlecase()
+save_file(student_name)
